ce98916662491422450533ff550312ab5d3d98fb
deepesh-np <nrndpsh@gmail.com>
2025-07-29 19:26:56 +0530

    media added

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 function Awards() {
-    return ( 
-        <h1>Awards</h1>
-     );
+  return (
+    <div className='container p-5 mb-5'>
+      <div className='row'>
+        <div className='col-lg-6 col-sm-4 col-sm-2'>
+          <div className='col-6'>
+            <img src="media/images/largestBroker.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className='container p-5 offset'>
+      <div className='row text-center'>
+        <img
+          src='media/images/homeHero.png'
+          alt='Hero Images'
+          className='mb-5'
+        />
+        <h1 className='mt-5'>Invest in Everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more.
+        </p>
+        <br />
+        <br />
+        <button
+          style={{ width: '20%', margin: '0 auto', backgroundColor: '#387ED1', color:"#FFFFFF" }}
+          className='p-2.75 btn fs-10'>
+          Signup up for free
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
