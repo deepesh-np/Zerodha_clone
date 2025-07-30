daac02e09a6477cb0615c3b464a280153326867a
deepesh-np <nrndpsh@gmail.com>
2025-07-30 23:01:17 +0530

    about done

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-    )
+  return (
+    <div className='container text-center'>
+      <div className='mt-5'>
+        <h2>Technology</h2>
+        <h3 style={{ fontSize: '1.0rem' }} className='text-muted mt-3'>
+          Sleek, modern and intutive trading platform
+        </h3>
+        <p className='mt-3'>
+          Check out our{' '}
+          <a href=''>
+            investment offering <i className='fa-solid fa-arrow-right'></i>{' '}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
