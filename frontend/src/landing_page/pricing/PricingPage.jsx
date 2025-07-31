d16235d305ea6ffe4cc1abb06f0c50b7041f78cd
deepesh-np <nrndpsh@gmail.com>
2025-07-31 22:59:25 +0530

    pricing done

--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -3,10 +3,12 @@
 import React from 'react';
 import Hero from './Hero';
 import Brokarage from './Brokarage';
+import OpenAccount from '../OpenAccount';
 function PricingPage() {
   return (
     <>
       <Hero />
+      <OpenAccount />
       <Brokarage />
     </>
   );
