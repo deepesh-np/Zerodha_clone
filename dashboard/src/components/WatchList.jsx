3f611c44867766a184abd19447e86403d9898a3a
deepesh-np <nrndpsh@gmail.com>
2025-08-10 22:54:48 +0530

    pre watchlist action setup

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,6 +1,10 @@
 /** @format */
 
-import React from 'react';
+import React, { use, useState } from 'react';
+
+import { Tooltip, Grow } from '@mui/material';
+import { watchlist } from '../data/data';
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 
 const WatchList = () => {
   return (
@@ -13,12 +17,47 @@ const WatchList = () => {
           placeholder='Search eg:infy, bse, nifty fut weekly, gold mcx'
           className='search'
         />
-        <span className='counts'> 9 / 50</span>
+        <span className='counts'> {watchlist.length} / 50</span>
       </div>
 
-      <ul className='list'></ul>
+      <ul className='list'>
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setshowWAtchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setshowWAtchlistActions(true);
+  };
+
+  const handleMouseExit = (e) => {
+    setshowWAtchlistActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter}>
+      <div className='item'>
+        <p className={stock.isDown ? 'down' : 'up'}> {stock.name} </p>
+        <div className='itemInfo'>
+          <span className='percent'> {stock.percent} </span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className='down' />
+          ) : (
+            <KeyboardArrowUp className='up' />
+          )}
+          <span className='price'> {stock.price} </span>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {};
